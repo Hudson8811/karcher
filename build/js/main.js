cf548fb37511fdbd733e8bdebea9592ef3c75a11
ArtZ <snickgem@gmail.com>
2020-08-20 12:25:36 +0400

    cards

--- build/js/main.js
+++ build/js/main.js
@@ -74,7 +74,9 @@ if(css3AnimationIsSupported&&o.allowCss3Support){animate()}else{_startAnimationW
 /* my scripts */
 
 $(document).ready(function () {
-	$('.first-screen__share').fancybox({
+	popupInit($('.first-screen__share'));
+	popupInit($('.first-screen__circle-wrapper--press'));
+	/*$('.first-screen__share').fancybox({
 		touch: false,
 		scrolling: 'no',
 		beforeShow: function(){
@@ -87,6 +89,28 @@ $(document).ready(function () {
 			smallBtn : '<div data-fancybox-close class="popup-share__close"></div>'
 		}
 	});
+
+	$('.first-screen__circle-wrapper--press')*/
+
+	$('.popup-share__refresh').click(function () {
+		$('.popup-share__card:visible').hide().siblings().eq(Math.floor(Math.random() * 6)).css('display', 'flex');
+	});
+
+	function popupInit(el) {
+		el.fancybox({
+			touch: false,
+			scrolling: 'no',
+			beforeShow: function(){
+				$("body").css({'overflow-y':'hidden'});
+			},
+			afterClose: function(){
+				$("body").css({'overflow-y':'visible'});
+			},
+			btnTpl : {
+				smallBtn : '<div data-fancybox-close class="popup-share__close"></div>'
+			}
+		});
+	}
 });
 $(document).ready(function () {
 	$('.header__burger').click(function () {
--- src/js/partials/components.js
+++ src/js/partials/components.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
-	$('.first-screen__share').fancybox({
+	popupInit($('.first-screen__share'));
+	popupInit($('.first-screen__circle-wrapper--press'));
+	/*$('.first-screen__share').fancybox({
 		touch: false,
 		scrolling: 'no',
 		beforeShow: function(){
@@ -12,4 +14,26 @@ $(document).ready(function () {
 			smallBtn : '<div data-fancybox-close class="popup-share__close"></div>'
 		}
 	});
+
+	$('.first-screen__circle-wrapper--press')*/
+
+	$('.popup-share__refresh').click(function () {
+		$('.popup-share__card:visible').hide().siblings().eq(Math.floor(Math.random() * 6)).css('display', 'flex');
+	});
+
+	function popupInit(el) {
+		el.fancybox({
+			touch: false,
+			scrolling: 'no',
+			beforeShow: function(){
+				$("body").css({'overflow-y':'hidden'});
+			},
+			afterClose: function(){
+				$("body").css({'overflow-y':'visible'});
+			},
+			btnTpl : {
+				smallBtn : '<div data-fancybox-close class="popup-share__close"></div>'
+			}
+		});
+	}
 });
